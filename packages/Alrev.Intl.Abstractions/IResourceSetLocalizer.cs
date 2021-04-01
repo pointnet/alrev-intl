20df60dfa9c5c1923fb5e6f5a3f844c6c58feb5b
pointnet <aurelien.verla@gmail.com>
2021-04-01 12:12:20 +0200

    [Alrev.Intl.Abstractions] - adding IResourceSetLocalizer<T>

---
+++ packages/Alrev.Intl.Abstractions/IResourceSetLocalizer.cs
@@ -0,0 +1,11 @@
+﻿using System.Globalization;
+
+namespace Alrev.Intl.Abstractions
+{
+    public interface IResourceSetLocalizer<T>
+    {
+        T CurrentLocalizer { get; }
+        T CurrentUILocalizer { get; }
+        T GetLocalizer(CultureInfo culture);
+    }
+}
