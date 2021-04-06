43033704773b00c084ff8a3941a2283d58b36360
pointnet <aurelien.verla@gmail.com>
2021-04-06 16:19:22 +0200

    [Alrev.Intl] - adding multiples target frameworks

---
+++ packages/Alrev.Intl.Abstractions/ReadOnlyDictionaryExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Alrev.Intl.Abstractions
+{
+    public static class ReadOnlyDictionaryExtensions
+    {
+        public static TValue GetValueOrDefault<TKey, TValue>(this ReadOnlyDictionary<TKey, TValue> dict, TKey key)
+             => dict.TryGetValue(key, out TValue value) ? value : default;
+        public static TValue GetValueOrDefault<TKey, TValue>(this ReadOnlyDictionary<TKey, TValue> dict, TKey key, TValue defaultValue)
+             => dict.TryGetValue(key, out TValue value) ? value : defaultValue;
+    }
+}
