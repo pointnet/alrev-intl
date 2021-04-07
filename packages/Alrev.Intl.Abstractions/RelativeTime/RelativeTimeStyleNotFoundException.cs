e4fbcbb1aca6ea95853e891325b3eaf404a7fd6c
pointnet <aurelien.verla@gmail.com>
2021-04-07 12:50:27 +0200

    [Alrev.Intl.Abstractions] - remove RelativeTime*Exception unused ctors

--- packages/Alrev.Intl.Abstractions/RelativeTime/RelativeTimeStyleNotFoundException.cs
+++ packages/Alrev.Intl.Abstractions/RelativeTime/RelativeTimeStyleNotFoundException.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Runtime.Serialization;
 
 namespace Alrev.Intl.Abstractions.RelativeTime
 {
@@ -9,54 +8,11 @@ namespace Alrev.Intl.Abstractions.RelativeTime
     public class RelativeTimeStyleNotFoundException : ArgumentException
     {
         /// <summary>
-        /// Initializes a new instance of the <see cref="RelativeTimeStyleNotFoundException"/> class with its message string set to a system-supplied message
-        /// </summary>
-        public RelativeTimeStyleNotFoundException() : base()
-        {
-        }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="RelativeTimeStyleNotFoundException"/> class with the specified error message
-        /// </summary>
-        /// <param name="message">The error message to display with this exception</param>
-        public RelativeTimeStyleNotFoundException(string message) : base(message)
-        {
-        }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="RelativeTimeStyleNotFoundException"/> class with a specified error message and a reference to the inner exception that is the cause of this exception
-        /// </summary>
-        /// <param name="message">The error message to display with this exception</param>
-        /// <param name="innerException">The exception that is the cause of the current exception. If the innerException parameter is not a null reference, the current exception is raised in a catch block that handles the inner exception</param>
-        public RelativeTimeStyleNotFoundException(string message, Exception innerException) : base(message, innerException)
-        {
-        }
-
-        /// <summary>
         /// Initializes a new instance of the <see cref="RelativeTimeStyleNotFoundException"/> class with a specified error message and the name of the parameter that is the cause this exception
         /// </summary>
         /// <param name="message">The error message to display with this exception</param>
         /// <param name="paramName">The name of the parameter that is the cause of the current exception</param>
         public RelativeTimeStyleNotFoundException(string message, string paramName) : base(message, paramName)
-        {
-        }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="RelativeTimeStyleNotFoundException"/> class with a specified error message, the invalid style, and a reference to the inner exception that is the cause of this exception
-        /// </summary>
-        /// <param name="message">The error message to display with this exception</param>
-        /// <param name="paramName">The name of the parameter that is the cause of the current exception</param>
-        /// <param name="innerException">The exception that is the cause of the current exception. If the innerException parameter is not a null reference, the current exception is raised in a catch block that handles the inner exception</param>
-        public RelativeTimeStyleNotFoundException(string message, string paramName, Exception innerException) : base(message, paramName, innerException)
-        {
-        }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="RelativeTimeStyleNotFoundException"/> class using the specified serialization data and context
-        /// </summary>
-        /// <param name="info">The object that holds the serialized object data</param>
-        /// <param name="context">The contextual information about the source or destination</param>
-        protected RelativeTimeStyleNotFoundException(SerializationInfo info, StreamingContext context) : base(info, context)
         {
         }
     }
--- packages/Alrev.Intl.Abstractions/RelativeTime/RelativeTimeUnitNotFoundException.cs
+++ packages/Alrev.Intl.Abstractions/RelativeTime/RelativeTimeUnitNotFoundException.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Runtime.Serialization;
 
 namespace Alrev.Intl.Abstractions.RelativeTime
 {
@@ -9,54 +8,11 @@ namespace Alrev.Intl.Abstractions.RelativeTime
     public class RelativeTimeUnitNotFoundException : ArgumentException
     {
         /// <summary>
-        /// Initializes a new instance of the <see cref="RelativeTimeUnitNotFoundException"/> class with its message string set to a system-supplied message
-        /// </summary>
-        public RelativeTimeUnitNotFoundException() : base()
-        {
-        }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="RelativeTimeUnitNotFoundException"/> class with the specified error message
-        /// </summary>
-        /// <param name="message">The error message to display with this exception</param>
-        public RelativeTimeUnitNotFoundException(string message) : base(message)
-        {
-        }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="RelativeTimeUnitNotFoundException"/> class with a specified error message and a reference to the inner exception that is the cause of this exception
-        /// </summary>
-        /// <param name="message">The error message to display with this exception</param>
-        /// <param name="innerException">The exception that is the cause of the current exception. If the innerException parameter is not a null reference, the current exception is raised in a catch block that handles the inner exception</param>
-        public RelativeTimeUnitNotFoundException(string message, Exception innerException) : base(message, innerException)
-        {
-        }
-
-        /// <summary>
         /// Initializes a new instance of the <see cref="RelativeTimeUnitNotFoundException"/> class with a specified error message and the name of the parameter that is the cause this exception
         /// </summary>
         /// <param name="message">The error message to display with this exception</param>
         /// <param name="paramName">The name of the parameter that is the cause of the current exception</param>
         public RelativeTimeUnitNotFoundException(string message, string paramName) : base(message, paramName)
-        {
-        }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="RelativeTimeUnitNotFoundException"/> class with a specified error message, the invalid unit, and a reference to the inner exception that is the cause of this exception
-        /// </summary>
-        /// <param name="message">The error message to display with this exception</param>
-        /// <param name="paramName">The name of the parameter that is the cause of the current exception</param>
-        /// <param name="innerException">The exception that is the cause of the current exception. If the innerException parameter is not a null reference, the current exception is raised in a catch block that handles the inner exception</param>
-        public RelativeTimeUnitNotFoundException(string message, string paramName, Exception innerException) : base(message, paramName, innerException)
-        {
-        }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="RelativeTimeUnitNotFoundException"/> class using the specified serialization data and context
-        /// </summary>
-        /// <param name="info">The object that holds the serialized object data</param>
-        /// <param name="context">The contextual information about the source or destination</param>
-        protected RelativeTimeUnitNotFoundException(SerializationInfo info, StreamingContext context) : base(info, context)
         {
         }
     }
