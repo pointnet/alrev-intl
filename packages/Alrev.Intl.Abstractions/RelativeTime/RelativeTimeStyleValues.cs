99fbcecd0715e973e1b74a6739a01dcb935fb6b7
pointnet <aurelien.verla@gmail.com>
2021-04-08 15:32:09 +0200

    [Alrev.Intl.RelativeTime] - adding RelativeTimeFormatter

--- packages/Alrev.Intl.Abstractions/RelativeTime/RelativeTimeStyleValues.cs
+++ packages/Alrev.Intl.Abstractions/RelativeTime/RelativeTimeStyleValues.cs
@@ -12,14 +12,18 @@
         /// <summary>
         /// Long style
         /// </summary>
+        /// <example>in 1 month</example>
         Long,
         /// <summary>
         /// Narrow style
         /// </summary>
+        /// <remarks> The narrow style could be similar to the short style for some locales</remarks>
+        /// <example>in 1mo.</example>
         Narrow,
         /// <summary>
         /// Short style
         /// </summary>
+        /// <example>in 1mo.</example>
         Short
     }
 }
