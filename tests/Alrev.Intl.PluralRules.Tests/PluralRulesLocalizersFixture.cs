e6449e31f0a45084fe0aa83d998209f78bd28afe
pointnet <aurelien.verla@gmail.com>
2021-04-11 14:22:28 +0200

    :hammer: [Alrev.Intl.PluralRules] - Update tests to support DI

--- tests/Alrev.Intl.PluralRules.Tests/PluralRulesLocalizersFixture.cs
+++ tests/Alrev.Intl.PluralRules.Tests/PluralRulesLocalizersFixture.cs
@@ -1,16 +1,18 @@
-﻿using Alrev.Intl.PluralRules.Globalization;
+﻿using Alrev.Intl.Abstractions;
+using Alrev.Intl.Abstractions.PluralRules;
+using Alrev.Intl.PluralRules.Globalization;
 
 namespace Alrev.Intl.PluralRules.Tests
 {
     public class PluralRulesLocalizersFixture
     {
-        public PluralRulesOrdinalLocalizer OrdinalLocalizer { get; private set; }
-        public PluralRulesCardinalLocalizer CardinalLocalizer { get; private set; }
+        public IResourceSetLocalizer<IPluralRulesResource> CardinalLocalizer { get; private set; }
+        public IResourceSetLocalizer<IPluralRulesResource> OrdinalLocalizer { get; private set; }
 
         public PluralRulesLocalizersFixture()
         {
-            this.OrdinalLocalizer = new PluralRulesOrdinalLocalizer();
             this.CardinalLocalizer = new PluralRulesCardinalLocalizer();
+            this.OrdinalLocalizer = new PluralRulesOrdinalLocalizer();
         }
     }
 }
