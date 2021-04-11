5ed499662e2b74bd2cca9d3bc55e0849491a5b87
pointnet <aurelien.verla@gmail.com>
2021-04-11 15:18:31 +0200

    :tada: [Alrev.Intl.PluralRules.DependencyInjection] - Add DI support for PluralRules

---
+++ tests/Alrev.Intl.PluralRules.DependencyInjection.Tests/ServiceCollectionExtensionsTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace Alrev.Intl.PluralRules.DependencyInjection.Tests
+{
+    public class ServiceCollectionExtensionsTests
+    {
+        [Fact]
+        public void AddIntlPluralRules_ShouldReturn_IServiceCollectionWithOneElement()
+        {
+            IServiceCollection services = new ServiceCollection();
+            services.AddIntlPluralRules();
+            Assert.Equal(1, services.Count);
+        }
+
+        [Fact]
+        public void AddIntlPluralRulesTwice_ShouldReturn_IServiceCollectionWithOneElement()
+        {
+            IServiceCollection services = new ServiceCollection();
+            services.AddIntlPluralRules();
+            services.AddIntlPluralRules();
+            Assert.Equal(1, services.Count);
+        }
+
+        [Fact]
+        public void AddIntlPluralRules_ShouldInject_ImplementationInstancePluralRulesEvaluator()
+        {
+            IServiceCollection services = new ServiceCollection();
+            services.AddIntlPluralRules();
+            Assert.IsType<PluralRulesEvaluator>(services[0].ImplementationInstance);
+        }
+
+        [Fact]
+        public void AddIntlPluralRules_ShouldInject_ServiceTypeIPluralRulesEvaluator()
+        {
+            IServiceCollection services = new ServiceCollection();
+            services.AddIntlPluralRules();
+            Assert.Equal("Alrev.Intl.Abstractions.PluralRules.IPluralRulesEvaluator", services[0].ServiceType.FullName);
+        }
+
+        [Fact]
+        public void AddIntlPluralRules_ShouldInject_LifetimeSingleton()
+        {
+            IServiceCollection services = new ServiceCollection();
+            services.AddIntlPluralRules();
+            Assert.Equal(ServiceLifetime.Singleton, services[0].Lifetime);
+        }
+    }
+}
