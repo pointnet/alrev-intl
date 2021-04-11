502e591111393ff0e00642fdf938c4fe28b069df
pointnet <aurelien.verla@gmail.com>
2021-04-11 14:22:41 +0200

    :hammer: [Alrev.Intl.RelativeTime] - Update tests to support DI

--- tests/Alrev.Intl.RelativeTime.Tests/RelativeTimeFormatterFixture.cs
+++ tests/Alrev.Intl.RelativeTime.Tests/RelativeTimeFormatterFixture.cs
@@ -5,10 +5,14 @@ namespace Alrev.Intl.RelativeTime.Tests
     public class RelativeTimeFormatterFixture
     {
         public IRelativeTimeFormatter Formatter { get; private set; }
+        public RelativeTimeLocalizerFixture LocalizerFixture { get; private set; }
+        public PluralRulesEvaluatorFixture EvaluatorFixture { get; private set; }
 
         public RelativeTimeFormatterFixture()
         {
-            this.Formatter = new RelativeTimeFormatter();
+            this.LocalizerFixture = new RelativeTimeLocalizerFixture();
+            this.EvaluatorFixture = new PluralRulesEvaluatorFixture();
+            this.Formatter = new RelativeTimeFormatter(this.LocalizerFixture.Localizer, this.EvaluatorFixture.Evaluator);
         }
     }
 }
