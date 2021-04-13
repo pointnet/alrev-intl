39ef9d018ccef0a700f9f725e07a4a60798590e8
pointnet <aurelien.verla@gmail.com>
2021-04-13 18:29:36 +0200

    :tada: [Alrev.Intl] - Add Alrev.Intl.Abstractions.Tests

---
+++ tests/Alrev.Intl.Abstractions.Tests/BaseResourceLocalizerTests.cs
@@ -0,0 +1,15 @@
+﻿using Alrev.Intl.Abstractions.Tests.Fixtures;
+using Xunit;
+
+namespace Alrev.Intl.Abstractions.Tests
+{
+    public class BaseResourceLocalizerTests
+    {
+        [Fact]
+        public void CtorWithNullDictionary_ShouldReturn_ValidInstanceWithEmptyDictionary()
+        {
+            BaseResourceLocalizerFixture brl = new BaseResourceLocalizerFixture(null);
+            Assert.Empty(brl);
+        }
+    }
+}
---
+++ tests/Alrev.Intl.Abstractions.Tests/Fixtures/BaseResourceLocalizerFixture.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Alrev.Intl.Abstractions.Tests.Fixtures
+{
+    public class BaseResourceLocalizerFixture : BaseResourceLocalizer<IIntlResource>
+    {
+        public BaseResourceLocalizerFixture(IDictionary<string, IIntlResource> dictionary) : base(dictionary)
+        {
+        }
+    }
+}
